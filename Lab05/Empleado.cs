c92c7d1042546d8914aded5bc0f1d4743881fe0c
LuisCorrales <luis.corrales@tecsup.edu.pe>
2024-04-13 15:50:23 -0500

    Agregar archivos de proyecto.

---
+++ Lab05/Empleado.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Controls.Primitives;
+
+namespace Lab05
+{
+    public class Empleado
+    {
+        public string Apellidos { get; set; }
+        public string Nombre { get; set; }
+        public string Cargo { get; set; }
+        public string Ciudad { get; set; }
+        public string Region { get; set; }
+    }
+}
---
+++ Lab05/ListEmp.xaml.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace Lab05
+{
+    public partial class ListEmp : Window
+    {
+        public ListEmp()
+        {
+            InitializeComponent();
+            string connectionString = "Server=(localdb)\\local;Database=Neptuno;Integrated Security=true;";
+            string query = "USP_ListarEmpleados";
+
+            List<Empleado> empleados = new List<Empleado>();
+
+            try
+            {
+                SqlConnection connection = new SqlConnection(connectionString);
+                connection.Open();
+                SqlCommand command = new SqlCommand(query, connection);
+                command.CommandType = CommandType.StoredProcedure;
+
+                SqlDataReader dataReader = command.ExecuteReader();
+                while (dataReader.Read())
+                {
+                    Empleado empleado = new Empleado();
+                    empleado.Apellidos = dataReader["Apellidos"].ToString();
+                    empleado.Nombre = dataReader["Nombre"].ToString();
+                    empleado.Cargo = dataReader["Cargo"].ToString();
+                    empleado.Ciudad = dataReader["Ciudad"].ToString();
+                    empleado.Region = dataReader["Region"].ToString();
+                    empleados.Add(empleado);
+                }
+                connection.Close();
+
+                dgEmpleados.ItemsSource = empleados;
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+        }
+    }
+}
